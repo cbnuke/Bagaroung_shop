c9b3436429bba4d40a58c17ec8ddc8ec32e14ea8
VoRDcs <VoRDcs@VoRD>
2014-06-15 05:49:27 +0700

    change -from user

--- application/views/admin/form_product_type.php
+++ application/views/admin/form_product_type.php
@@ -14,24 +14,39 @@
     $attributes = array('class' => 'form-horizontal', 'id' => 'form_product_type');
     if ($mode == 'edit') {
         echo form_open('', $attributes);
-        foreach ($type as $r) {
-            $name_th = $r['product_type']['thai'];
-            $name_en = $r['product_type']['english'];
-        }
+        $name_th = $type['product_type']['thai'];
+        $name_en = $type['product_type']['english'];
     } else {
         echo form_open('ProductTypes/add', $attributes);
+        $name_th = set_value('type[thai]');
+        $name_en = set_value('type[english]');
     }
-    ?>   
-    <div class="form-group">
-        <label class="col-sm-2 control-label">ชื่อประเภทสินค้า(TH)</label>
-        <div class="col-sm-6">
-            <input type="text" class="form-control" name="type[thai]" id="" placeholder="ชื่อประเภทสินค้า" required="" value="<?= $name_th ?>">
+    ?>     
+    <div class="form-group" id="error">
+        <label class="col-sm-2 control-label"></label>
+        <div class="col-sm-4">
+            <div class="alert alert-danger">
+                <?= validation_errors(); ?>
+            </div>
         </div>
     </div>
     <div class="form-group">
+        <label class="col-sm-2 control-label">ชื่อประเภทสินค้า(TH)</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" name='type[thai]' id="" placeholder="ชื่อประเภทสินค้า" value="<?= $name_th ?>">
+        </div>
+        <div class="col-sm-4" id="error">                
+            <?php echo form_error('type[thai]', '<font color="error">', '</font>'); ?>
+        </div>
+    </div>
+
+    <div class="form-group">
         <label class="col-sm-2 control-label">ชื่อประเภทสินค้า(EN)</label>
         <div class="col-sm-6">
-            <input type="text" class="form-control" name="type[english]" id="" placeholder="Product Type Name" required="" value="<?= $name_en ?>">
+            <input type="text" class="form-control" name="type[english]" id="" placeholder="Product Type Name"  value="<?= $name_en ?>">
+        </div>
+        <div class="col-sm-4" id="error">                
+            <?php echo form_error('type[english]', '<font color="error">', '</font>'); ?>
         </div>
     </div>   
     <hr>
@@ -39,11 +54,24 @@
         <label class="col-sm-2 control-label"></label>
         <div class="col-sm-6" align="center">
             <input type="submit"  class="btn btn-success btn-lg" name="save" value="บันทึก" >
-<?= anchor('ProductTypes', 'ยกเลิก', 'class="btn btn-danger btn-lg"') ?>            
+            <?= anchor('ProductTypes', 'ยกเลิก', 'class="btn btn-danger btn-lg"') ?>            
         </div>
     </div>
 
-<?= form_close(); ?>
+    <?= form_close(); ?>
 
 </div>
-
+<?= js('jquery.js'); ?>
+<script>
+    $(document).ready(function() {
+        var er = '<?=validation_errors()?>';
+//        alert(mode);
+        if (er == '')
+        {
+            $('#error').hide(true);
+        } else
+        {
+            $('#error').show();
+        }
+    });
+</script>  
