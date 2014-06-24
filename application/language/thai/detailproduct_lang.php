6f04da6b6df4490b246d9bbd7504d0cf32a387da
CBNUKE <jimmy29922@gmail.com>
2014-06-25 03:32:53 +0700

    Forget upload lang

---
+++ application/language/thai/detailproduct_lang.php
@@ -0,0 +1,24 @@
+<?php
+$lang['welcome'] = "สวัสดีครับ";
+$lang['menu_home'] = "หน้าหลัก";
+$lang['menu_products'] = "ผลิตภัณฑ์";
+$lang['menu_promotions'] = "โปรโมชั่น";
+$lang['menu_contactus'] = "ติดต่อ";
+$lang['head_promotions'] = "โปรโมชั่น";
+$lang['head_products'] = "ผลิตภัณฑ์";
+$lang['head_contactus'] = "ติดต่อ";
+
+$lang['recommend'] = "สินค้าแนะนำ";
+$lang['how_to_order'] = "วิธีการสั่งซื้อ";
+
+$lang['detail_product'] = "รายละเอียด";
+$lang['product_price'] = "ราคา";
+$lang['baht'] = "บาท";
+$lang['product_size'] = "ขนาด";
+$lang['product_weight'] = "น้ำหนัก";
+$lang['product_width'] = "ความกว้าง";
+$lang['product_high'] = "ความสูง";
+$lang['product_top_width'] = "ความกว้างด้านบน";
+$lang['product_base_width'] = "ความกว้างฐาน";
+$lang['kg'] = "กก.";
+$lang['cm'] = "ซม.";
